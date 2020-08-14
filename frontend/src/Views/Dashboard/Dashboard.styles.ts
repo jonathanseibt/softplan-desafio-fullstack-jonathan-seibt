418fb142d6681ecded22e0e39ba4fc594c365292
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 05:22:12 -0300

    Layout da tela de dashboard.

--- frontend/src/Views/Dashboard/Dashboard.styles.ts
+++ frontend/src/Views/Dashboard/Dashboard.styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   container: {
     width: "100%",
-    height: "50%",
+    height: "75%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
