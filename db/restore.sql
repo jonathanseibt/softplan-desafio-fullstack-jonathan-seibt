b2f700c19c491dfaaa09c56f864ef509939c13ba
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 20:27:30 -0300

    Restauração do banco de dados.

---
+++ db/restore.sql
@@ -0,0 +1,16 @@
+use softplan;
+
+insert into user
+  (id, name, email, role)
+values
+  (1, 'Carlos', 'administrador@email.com', 1);
+
+insert into user
+  (id, name, email, role)
+values
+  (2, 'Ilson', 'triador@email.com', 2);
+
+insert into user
+  (id, name, email, role)
+values
+  (3, 'Moacir', 'finalizador@email.com', 3);
