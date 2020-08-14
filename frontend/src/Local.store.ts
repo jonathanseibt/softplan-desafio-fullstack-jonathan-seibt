d5bf8d9f28884e6c205c2804188f81a0e3466043
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 01:15:02 -0300

    Informações do usuário em local storage.

--- frontend/src/Local.store.ts
+++ frontend/src/Local.store.ts
@@ -2,15 +2,15 @@ import { observable } from "mobx";
 import { persist } from "mobx-persist";
 
 interface User {
-  id: 0;
-  name: "";
-  email: "";
-  role: 0;
+  id: number;
+  name: string;
+  email: string;
+  role: number;
 }
 
 class Store {
   @persist @observable isAuthenticated = false;
-  @persist @observable user: User | null = null;
+  @persist @observable user: User | null = { id: 0, name: "", email: "", role: 0 };
 }
 
 export default new Store();
