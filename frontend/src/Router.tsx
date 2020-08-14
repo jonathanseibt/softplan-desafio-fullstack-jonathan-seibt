112376e349762e784c232d962fbfd3dd18555ccd
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 03:59:16 -0300

    Melhoria nas rotas.

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -5,48 +5,53 @@ import DashboardLayout from "./Layouts/Dashboard/Dashboard.layout";
 import AuthenticationLayout from "./Layouts/Authentication/Authentication.layout";
 import DashboardView from "./Views/Dashboard/Dashboard.view";
 import LoginView from "./Views/Login/Login.view";
+import UsuariosView from "./Views/Usuarios/Usuarios.view";
 import LocalStore from "./Local.store";
 
 const Router: React.FC = observer(() => {
-  const isAuthenticatedRoutes = () => {
-    return (
-      <DashboardLayout>
-        <Route exact path="/">
-          <Redirect to="/dashboard" />
-        </Route>
-
-        <Route path="/dashboard">
-          <DashboardView />
-        </Route>
-
-        <Route path="*">
-          <Redirect to="/dashboard" />
-        </Route>
-      </DashboardLayout>
-    );
-  };
-
-  const isNotAuthenticatedRoutes = () => {
-    return (
-      <AuthenticationLayout>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route>
-
-        <Route path="/login">
-          <LoginView />
-        </Route>
-
-        <Route path="*">
-          <Redirect to="/login" />
-        </Route>
-      </AuthenticationLayout>
-    );
-  };
-
   return (
     <BrowserRouter>
-      <Switch>{LocalStore.isAuthenticated ? isAuthenticatedRoutes() : isNotAuthenticatedRoutes()}</Switch>
+      {LocalStore.hydrated && (
+        <Switch>
+          <Route exact path="/">
+            {LocalStore.isAuthenticated ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+          </Route>
+
+          <Route path="/login">
+            {LocalStore.isAuthenticated ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <AuthenticationLayout>
+                <LoginView />
+              </AuthenticationLayout>
+            )}
+          </Route>
+
+          <Route path="/dashboard">
+            {LocalStore.isAuthenticated ? (
+              <DashboardLayout>
+                <DashboardView />
+              </DashboardLayout>
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+
+          <Route path="/usuarios">
+            {LocalStore.isAuthenticated ? (
+              <DashboardLayout>
+                <UsuariosView />
+              </DashboardLayout>
+            ) : (
+              <Redirect to="/" />
+            )}
+          </Route>
+
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      )}
     </BrowserRouter>
   );
 });
