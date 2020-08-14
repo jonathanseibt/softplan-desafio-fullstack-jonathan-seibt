d681a12004d5ac845f39cad9fc45f656c86dc73e
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 01:02:52 -0300

    Informações do usuário em local storage.

--- frontend/src/Local.store.ts
+++ frontend/src/Local.store.ts
@@ -1,8 +1,16 @@
 import { observable } from "mobx";
 import { persist } from "mobx-persist";
 
+interface User {
+  id: 0;
+  name: "";
+  email: "";
+  role: 0;
+}
+
 class Store {
   @persist @observable isAuthenticated = false;
+  @persist @observable user: User | null = null;
 }
 
 export default new Store();
