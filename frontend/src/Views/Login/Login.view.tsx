f67403b24e6e324ffbef5ef9029792524aad7144
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 01:06:15 -0300

    Tela de login (ainda sem chamada para o backend).

--- frontend/src/Views/Login/Login.view.tsx
+++ frontend/src/Views/Login/Login.view.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { observer } from "mobx-react";
+import { TextField, Button, Box } from "@material-ui/core";
+import LocalStore from "../../Local.store";
 
 const View: React.FC = observer(() => {
-  return <>Login.view.tsx</>;
+  const onClickLogin = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    LocalStore.isAuthenticated = true;
+  };
+
+  return (
+    <>
+      <TextField variant="outlined" margin="normal" required fullWidth label="Email" name="email" autoComplete="email" autoFocus />
+
+      <TextField variant="outlined" margin="normal" required fullWidth label="Senha" name="senha" autoComplete="current-password" type="password" />
+
+      <Box marginTop={2}>
+        <Button type="submit" fullWidth variant="contained" color="primary" onClick={onClickLogin}>
+          Login
+        </Button>
+      </Box>
+    </>
+  );
 });
 
 export default View;
