20080b4662d485d233f5acfe09631f0226126083
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 01:01:26 -0300

    Layout da tela de login.

--- frontend/src/Layouts/Authentication/Authentication.layout.tsx
+++ frontend/src/Layouts/Authentication/Authentication.layout.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { observer } from "mobx-react";
+import { Container, Avatar, Box, Typography } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 const Layout: React.FC = observer((_props) => {
   return (
-    <>
-      <div>Authentication.layout.tsx</div>
-      {_props.children}
-    </>
+    <Container component="main" maxWidth="xs">
+      <Box marginX={4} marginY={8}>
+        <Box marginBottom={2}>
+          <Box display="flex" justifyContent="center">
+            <Avatar>
+              <LockOutlinedIcon />
+            </Avatar>
+          </Box>
+
+          <Typography component="h1" variant="h5" align="center">
+            Login
+          </Typography>
+        </Box>
+
+        <form noValidate>{_props.children}</form>
+      </Box>
+    </Container>
   );
 });
 
