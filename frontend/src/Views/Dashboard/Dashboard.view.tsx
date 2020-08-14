7fad68d21b719f9c159337468f3e69a2ad08eed7
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-08-14 01:45:44 -0300

    Tela de dashboard.

--- frontend/src/Views/Dashboard/Dashboard.view.tsx
+++ frontend/src/Views/Dashboard/Dashboard.view.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { observer } from "mobx-react";
+import { Box, Typography } from "@material-ui/core";
+import LocalStore from "../../Local.store";
 
 const View: React.FC = observer(() => {
-  return <>Dashboard.view.tsx</>;
+  return (
+    <Box width="100%" height="50%" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+      <Typography component="h2" variant="h1">
+        Olá,
+      </Typography>
+      <Typography component="h3" variant="h2">
+        {LocalStore.userName}
+      </Typography>
+    </Box>
+  );
 });
 
 export default View;
